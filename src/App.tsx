628b5127978ab1773d668fbb3c7a6ec3d789720c
deanchen <antlet2004@gmail.com>
2020-12-27 15:22:31 +0800

    layout of calculator and basic appearance of the app is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from "@material-ui/icons/Search";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from '@material-ui/core/styles';
+import Icon from "./iconTemplate.png";
+import Calculator from "./AppIcon_128x128x32.png";
+import Box from "@material-ui/core/Box";
+import Divider from "@material-ui/core/Divider";
+
+
+const HEIGHT = 56;
+const BACKGROUND_COLOR = "#5B5B5B"
+const GRAY_COLOR = "#D5D1CA"
+const GRAY_COLOR_2 = "#a5a39e"
+
+const useStyles = makeStyles({
+  root: {
+    background: BACKGROUND_COLOR,
+  },
+  headBox:{
+    justifyContent: "space-between",
+    height: HEIGHT ,
+  },
+  searchBox:{
+    width: HEIGHT,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  searchIcon: {
+    fontSize: 32,
+    fill: GRAY_COLOR,
+  },
+  inputBox:{
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexGrow: 1,
+  },
+  input: {
+    width: "100%",
+    color: GRAY_COLOR,
+    fontSize: 32,
+    border: "0px solid white",
+    outline: "none",
+    background: BACKGROUND_COLOR,
+  },
+  divider:{
+    background: GRAY_COLOR_2,
+  },
+  box2: {
+  },
+  box3: {
+    flexWrap: "nowrap",
+    flexDirection: "row",
+    minHeight: 500,
+  },
+  box4: {
+  },
+  calBox: {
+    height: "100%",
+    flexDirection: "column",
+    padding: 20,
+    flexWrap: "nowrap",
+  },
+  calBoxUp: {
+    height: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    padding: 10,
+    color: GRAY_COLOR,
+  },
+  calBoxDown: {
+    height: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    padding: 10,
+    color: GRAY_COLOR,
+  },
+});
+
+
+
+//dev
+if(!window.require){
+  //@ts-ignore
+  window.require= function(){
+    return {
+      ipcRenderer: {
+        invoke: () => {
+          console.log("mock invoke");
+          return Promise.resolve(100);
+        }
+      }
+    };
+  }
+}
 
 const ipc = window.require("electron").ipcRenderer;
 const {shell} = window.require("electron");
@@ -21,6 +120,8 @@ function App() {
   const refInput = React.useRef(null);
   const [input, setInput] = React.useState("");
   const [output, setOutput] = React.useState("");
+  const classes = useStyles();
+  const [mode, setMode] = React.useState("");
 
   function handleKeyUp(e: any){
       if(e.key === "Enter"){
@@ -39,9 +140,12 @@ function App() {
     cal(e.target.value)
       .then((r: any) => {
         if(r){
-          setOutput(r);
+          console.log("r:", r);
+          setOutput(r + "");
+          setMode("calculator");
         }else{
           setOutput('');
+          setMode("");
         }
       });
   }
@@ -68,19 +172,64 @@ function App() {
 //    });
 //  }
   return (
-    <div style={{background:"white"}}>
-      <input 
-        ref={refInput} 
-        onChange={handleChange} 
-        onKeyUp={handleKeyUp}
-        style={{fontSize: 25}} 
-        value={input} />
-      <span 
-        style={{
-          marginTop: 5,
-          fontSize:25}}
-      >{output}</span>
-    </div>
+    <Grid container className={classes.root} direction="column" >
+      <Grid item>
+        <Grid container className={classes.headBox} >
+          <Grid item className={classes.searchBox} >
+            <Search className={classes.searchIcon} />
+          </Grid>
+          <Grid item className={classes.inputBox} >
+            <input 
+              className={classes.input}
+              placeholder="Dadior Search"
+              ref={refInput} 
+              onChange={handleChange} 
+              onKeyUp={handleKeyUp}
+              style={{fontSize: 25}} 
+              value={input} />
+          </Grid>
+          <Grid item className={classes.searchBox} >
+            {mode === "" &&
+              <Box/>
+            }
+            {mode === "calculator" &&
+              <Avatar variant="square" src={Calculator} />
+            }
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <Divider className={classes.divider} />
+      </Grid>
+      <Grid item className={classes.box2} >
+        <Grid container className={classes.box3} >
+          <Grid item xs={4} >
+          </Grid>
+          <Grid item>
+            <Divider className={classes.divider} orientation="vertical" />
+          </Grid>
+          <Grid item xs={8} >
+            {mode === "calculator" &&
+              <Grid container className={classes.calBox} >
+                <Grid item className={classes.calBoxUp} >
+                  <Typography variant="h5" >
+                    {input} = 
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Divider className={classes.divider} />
+                </Grid>
+                <Grid item className={classes.calBoxDown} >
+                  <Typography variant="h4" >
+                    <span>{output}</span>
+                  </Typography>
+                </Grid>
+              </Grid>
+            }
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
